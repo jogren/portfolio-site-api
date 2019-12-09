0bf50d310dffa7f4392275f9855d816b2658a0da
Jacob Ogren <jogren0044@gmail.com>
2019-12-09 08:48:34 -0700

    Install cors middleware

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const cors = require('cors')
 
 app.set('port', process.env.PORT || 3000);
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (request, response) => {
